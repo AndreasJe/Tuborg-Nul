7ceec75e7644a7d1419ce95e990a9448e832da16
AndreasJe <gud.er.gud@gmail.com>
2020-03-19 03:35:20 +0100

    Eks. på første side med placeholders. Ellers pynt, media queries, nye fonte og flere af dem, grafik og struktur.
    Sikrer at sitet fungerer fremadrettet, når vi tilføjer content.
    Mangler dog tilpasning af tablet.
    
    Om ikke andet, en brugbar template!

--- script.js
+++ script.js
@@ -7,6 +7,47 @@ document.onreadystatechange = function() {
       document.getElementById('interactive');
       document.getElementById('load').style.visibility = "hidden";
       document.getElementById('mainWrapper').style.visibility = "visible";
-    }, 4500);
+    }, 500);
   }
 }
+
+
+
+//
+// // SCROLL LOOP FUNKTION. SER EFTER OM ET ELEMENT ER I VIEWPORT OG DEREFTER TILDELER DET EN CLASS. KAN BRUGES PÅ MANGE MÅDER
+//
+// var elementsToShow = document.querySelectorAll('.show-on-scroll');
+//
+// function loop() {
+//
+//   elementsToShow.forEach(function(element) {
+//     if (isElementInViewport(element)) {
+//       element.classList.add('is-visible');
+//     } else {
+//       element.classList.remove('is-visible');
+//     }
+//   });
+//
+//   scroll(loop);
+// }
+//
+// loop();
+// console.log("scroll-loop loaded");
+//
+// // Helper function from: http://stackoverflow.com/a/7557433/274826
+// function isElementInViewport(el) {
+//   // special bonus for those using jQuery
+//   if (typeof jQuery === "function" && el instanceof jQuery) {
+//     el = el[0];
+//   }
+//   var rect = el.getBoundingClientRect();
+//   return (
+//     (rect.top <= 0 &&
+//       rect.bottom >= 0) ||
+//     (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+//       rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+//     (rect.top >= 0 &&
+//       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+//   );
+// }
+// console.log("isElementInViewport loaded");
